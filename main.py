8325b48fd41e04e483a35e9ad2d46d32a51606cb
HARSH <harsh.v.s.1021@gmail.com>
2026-06-20 15:57:53 +0530

    Updated the directory and added more feature to the main file

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ class Library:
             content = f.read().strip()
             if content:
                 data = json.loads(content)
-    else: 
-        with open(database , "w") as f:
-            json.dump(data , f ,indent=2)
+    # else: 
+    #     with open(database , "w") as f:
+    #         json.dump(data , f ,indent=2)
 
     @classmethod
     def save_data(cls):# because i don't want anyone do perform this operation outside the class , and class method are only accesed inside the class
@@ -49,6 +49,37 @@ class Library:
         Library.data['books'].append(book)
         Library.save_data()
 
+    def list_books(self):
+        if not Library.data["books"]:
+            print("No book is available right now ")
+            return 
+        for b in Library.data["books"]:
+            print(f"{b['id']:12}{b['title'][:20]:12} {b['author'][:24]}")
+
+    def add_member(self):
+        name = input("enter name of your member: ")
+        email = input("enter the mail: ")
+        member = {
+            "id": Library.gen_id("M"),
+            "name": name,
+            "email" : email,
+            "borrowed" : []
+
+        }
+        Library.data['members'].append(member)
+        Library.save_data()
+        print(member)
+        print("New member added successfully ")
+
+    def list_members(self):
+        if not Library.data['members']:
+            print("No member right now ")
+            return
+        for m in Library.data['members']:
+            print(f"{m['id']:12} {m['name']:12} {m['email']:12}")
+            print(f"Books borrowed are {m['borrowed']}")
+
+    
 
 
 obj = Library()
@@ -66,4 +97,13 @@ print("-"*50)
 choice = int(input("What task you want to do? "))
 if choice == 1:
     obj.add_book()
-    
+elif choice == 2:
+    obj.list_books()
+elif choice == 3:
+    obj.add_member()
+elif choice == 4:
+    obj.list_members()
+elif choice == 5:
+    obj.borrow_book()
+elif choice == 0:
+    obj.exit_portal()
